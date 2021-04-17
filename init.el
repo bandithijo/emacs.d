58936a8a0c605b9439fa1bda092ebff1de7eec02
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-04-17 10:00:39 +0800

    Initial commit

---
+++ init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(package-selected-packages
+   '(projectile dashboard solaire-mode doom-themes general doom-modeline which-key vscode-dark-plus-theme use-package evil-collection)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
